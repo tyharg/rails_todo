340a57e798bf2917115026d981fa364b699185e1
tyharg <tytom22@gmail.com>
2021-11-30 12:27:36 -0600

    Add User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   resources :todo_lists, :path => "todo_list"
 
-root "todo_lists#index"
+  root "todo_lists#index"
+  
+  get '/login', to: 'sessions#login'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy'
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_23_225721) do
+ActiveRecord::Schema.define(version: 2021_11_25_090132) do
 
   create_table "todo_lists", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "due"
     t.boolean "completed"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
